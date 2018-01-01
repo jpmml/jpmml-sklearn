660bf26ebd50de2e3f293c82daed5493d1e0a1fb
Villu Ruusmann <villu.ruusmann@gmail.com>
2018-01-01 16:21:48 +0200

    Improved support for Python-only selector types

---
+++ src/main/java/sklearn/feature_selection/PySelector.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2018 Villu Ruusmann
+ *
+ * This file is part of JPMML-SkLearn
+ *
+ * JPMML-SkLearn is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JPMML-SkLearn is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with JPMML-SkLearn.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package sklearn.feature_selection;
+
+import java.util.List;
+
+import org.jpmml.sklearn.ClassDictUtil;
+import sklearn.Selector;
+import sklearn2pmml.SelectorProxy;
+
+public class PySelector extends Selector {
+
+	public PySelector(String module, String name){
+		super(module, name);
+	}
+
+	@Override
+	public int getNumberOfFeatures(){
+		throw new IllegalArgumentException(formatMessage());
+	}
+
+	@Override
+	public List<Boolean> getSupportMask(){
+		throw new IllegalArgumentException(formatMessage());
+	}
+
+	private String getName(){
+		return (String)get("__class__");
+	}
+
+	private String getSimpleName(){
+		String name = getName();
+
+		int dot = name.lastIndexOf('.');
+		if(dot > -1){
+			return name.substring(dot + 1);
+		}
+
+		return name;
+	}
+
+	private String formatMessage(){
+		return "The selector object (" + ClassDictUtil.formatClass(this) + ") does not have persistent state. " +
+			"Please use the " + (SelectorProxy.class).getName() + " wrapper class to give the selector object a persistent state (eg. " + (SelectorProxy.class).getSimpleName() + "(" + getSimpleName() + "(...)))";
+	}
+}
