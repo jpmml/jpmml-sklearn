ebd4aed7aeeea9a34e409362b172eab326b065dd
Villu Ruusmann <villu.ruusmann@gmail.com>
2019-12-06 16:59:11 +0200

    Improved the registration of converter classes

--- src/main/java/org/jpmml/sklearn/PickleUtil.java
+++ src/main/java/org/jpmml/sklearn/PickleUtil.java
@@ -23,7 +23,11 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.Properties;
 import java.util.Set;
 
@@ -145,50 +149,82 @@ public class PickleUtil {
 		for(String key : keys){
 			String value = properties.getProperty(key);
 
-			if(value == null || ("").equals(value)){
-				value = key;
+			Collection<String> simpleKeys = expandComplexKey(key);
+			for(String simpleKey : simpleKeys){
+				init(classLoader, simpleKey, value);
 			}
-
-			logger.debug("Mapping Python class {} to Java class {}", key, value);
-
-			int dot = key.lastIndexOf('.');
-			if(dot < 0){
-				logger.warn("Failed to identify the module and name parts of Python class {}", key);
-
-				continue;
-			}
-
-			String module = key.substring(0, dot);
-			String name = key.substring(dot + 1);
-
-			Class<?> clazz;
-
-			try {
-				clazz = classLoader.loadClass(value);
-			} catch(ClassNotFoundException cnfe){
-				logger.warn("Failed to load Java class {}", value);
-
-				continue;
-			}
-
-			ObjectConstructor constructor;
-
-			if((CClassDict.class).isAssignableFrom(clazz)){
-				constructor = new ExtensionObjectConstructor(module, name, (Class<? extends CClassDict>)clazz);
-			} else
-
-			if((PyClassDict.class).isAssignableFrom(clazz)){
-				constructor = new ObjectConstructor(module, name, (Class<? extends PyClassDict>)clazz);
-			} else
-
-			{
-				logger.warn("Failed to identify the type of Java class {}", value);
-
-				continue;
-			}
-
-			Unpickler.registerConstructor(constructor.getModule(), constructor.getName(), constructor);
 		}
+	}
+
+	static
+	private void init(ClassLoader classLoader, String key, String value){
+
+		if(value == null || ("").equals(value)){
+			value = key;
+		}
+
+		logger.debug("Mapping Python class {} to Java class {}", key, value);
+
+		int dot = key.lastIndexOf('.');
+		if(dot < 0){
+			logger.warn("Failed to identify the module and name parts of Python class {}", key);
+
+			return;
+		}
+
+		String module = key.substring(0, dot);
+		String name = key.substring(dot + 1);
+
+		Class<?> clazz;
+
+		try {
+			clazz = classLoader.loadClass(value);
+		} catch(ClassNotFoundException cnfe){
+			logger.warn("Failed to load Java class {}", value);
+
+			return;
+		}
+
+		ObjectConstructor constructor;
+
+		if((CClassDict.class).isAssignableFrom(clazz)){
+			constructor = new ExtensionObjectConstructor(module, name, (Class<? extends CClassDict>)clazz);
+		} else
+
+		if((PyClassDict.class).isAssignableFrom(clazz)){
+			constructor = new ObjectConstructor(module, name, (Class<? extends PyClassDict>)clazz);
+		} else
+
+		{
+			logger.warn("Failed to identify the type of Java class {}", value);
+
+			return;
+		}
+
+		Unpickler.registerConstructor(constructor.getModule(), constructor.getName(), constructor);
+	}
+
+	static
+	private Collection<String> expandComplexKey(String key){
+		int begin = key.indexOf('(');
+		int end = key.indexOf(')', begin + 1);
+
+		if(begin < 0 || end < 0){
+			return Collections.singletonList(key);
+		}
+
+		String prefix = key.substring(0, begin);
+		String body = key.substring(begin + 1, end);
+		String suffix = key.substring(end + 1);
+
+		List<String> result = new ArrayList<>();
+
+		String[] strings = body.split("\\|");
+		for(String string : strings){
+			result.add(prefix + string + suffix);
+		}
+
+		return result;
 	}
 
 	private static final Logger logger = LoggerFactory.getLogger(PickleUtil.class);
