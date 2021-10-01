4ec7ddfa833eb7ae649f3674072b4fe7cc28b5ad
Villu Ruusmann <villu.ruusmann@gmail.com>
2021-10-01 13:32:04 +0300

    Updated test resources

---
+++ src/test/java/org/jpmml/sklearn/ModelStatsTest.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2021 Villu Ruusmann
+ *
+ * This file is part of JPMML-SkLearn
+ *
+ * JPMML-SkLearn is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JPMML-SkLearn is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with JPMML-SkLearn.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.jpmml.sklearn;
+
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.function.Predicate;
+
+import com.google.common.base.Equivalence;
+import org.dmg.pmml.Model;
+import org.dmg.pmml.ModelStats;
+import org.dmg.pmml.PMML;
+import org.dmg.pmml.PMMLObject;
+import org.dmg.pmml.Visitor;
+import org.dmg.pmml.VisitorAction;
+import org.jpmml.evaluator.ResultField;
+import org.jpmml.model.visitors.AbstractVisitor;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ModelStatsTest extends SkLearnTest implements Algorithms, Datasets {
+
+	@Test
+	public void checkGBDTLRAudit() throws Exception {
+		check(GBDT_LR, AUDIT);
+	}
+
+	@Test
+	public void checkTPOTAuto() throws Exception {
+		check("TPOT", AUTO);
+	}
+
+	@Test
+	public void checkIsolationForestHousing() throws Exception {
+		check(ISOLATION_FOREST, HOUSING);
+	}
+
+	@Test
+	public void checkEstimatorTransformerWheat() throws Exception {
+		check("EstimatorTransformer", WHEAT);
+	}
+
+	public void check(String name, String dataset) throws Exception {
+		Predicate<ResultField> predicate = (resultField) -> true;
+		Equivalence<Object> equivalence = getEquivalence();
+
+		try(SkLearnTestBatch batch = (SkLearnTestBatch)createBatch(name, dataset, predicate, equivalence)){
+			check(batch);
+		}
+	}
+
+	private void check(SkLearnTestBatch batch) throws Exception {
+		PMML pmml = batch.getPMML();
+
+		Visitor visitor = new AbstractVisitor(){
+
+			@Override
+			public VisitorAction visit(ModelStats modelStats){
+				Deque<PMMLObject> parents = getParents();
+
+				Iterator<PMMLObject> it = parents.iterator();
+
+				assertTrue(it.next() instanceof Model);
+				assertTrue(it.next() instanceof PMML);
+
+				return super.visit(modelStats);
+			}
+		};
+		visitor.applyTo(pmml);
+	}
+}
