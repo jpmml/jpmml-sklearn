79dca0a6e9fffa54624d8cce5b3fb312a5e02e33
Villu Ruusmann <villu.ruusmann@gmail.com>
2024-03-10 11:24:29 +0200

    Ensured compatibility with Scikit-Learn 1.4.1.post1

--- pmml-sklearn/src/main/java/sklearn/Step.java
+++ pmml-sklearn/src/main/java/sklearn/Step.java
@@ -37,9 +37,9 @@ public class Step extends PythonObject implements HasNumberOfFeatures, HasType {
 	public void checkSkLearnVersion(){
 		String sklearnVersion = getSkLearnVersion();
 
-		if(sklearnVersion != null && VersionUtil.compareVersion(sklearnVersion, "1.4.0") > 0){
+		if(sklearnVersion != null && VersionUtil.compareVersion(sklearnVersion, "1.4.1") > 0){
 			String message = "This converter version does not know about Scikit-Learn version " + sklearnVersion + " artifacts. " +
-				"Please upgrade the converter to the latest version, or downgrade Scikit-Learn to version " + "1.4.0";
+				"Please upgrade the converter to the latest version, or downgrade Scikit-Learn to version " + "1.4.1";
 
 			throw new IllegalArgumentException(message);
 		}
