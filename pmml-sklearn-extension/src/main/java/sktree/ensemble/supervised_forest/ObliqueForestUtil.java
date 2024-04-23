b3d2e3171454c72ed52aba99f7121e8d5a14bf5b
Villu Ruusmann <villu.ruusmann@gmail.com>
2024-04-23 07:35:31 +0300

    Renamed 'sktree.ensemble.forest' package to 'sktree.ensemble.supervised_forest'

--- pmml-sklearn-extension/src/main/java/sktree/ensemble/supervised_forest/ObliqueForestUtil.java
+++ pmml-sklearn-extension/src/main/java/sktree/ensemble/supervised_forest/ObliqueForestUtil.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with JPMML-SkLearn.  If not, see <http://www.gnu.org/licenses/>.
  */
-package sktree.ensemble.forest;
+package sktree.ensemble.supervised_forest;
 
 import java.util.List;
 import java.util.function.Function;
