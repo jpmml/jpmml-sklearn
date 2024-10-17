c108f6ac870d4da940f35fb134c418eb99a44473
Villu Ruusmann <villu.ruusmann@gmail.com>
2024-10-17 21:58:53 +0300

    Ensured compatibility with Scikit-Learn 1.5.2

--- pmml-sklearn/src/main/java/sklearn/Step.java
+++ pmml-sklearn/src/main/java/sklearn/Step.java
@@ -38,9 +38,9 @@ public class Step extends PythonObject implements HasNumberOfFeatures, HasType {
 	public void checkSkLearnVersion(){
 		String sklearnVersion = getSkLearnVersion();
 
-		if(sklearnVersion != null && VersionUtil.compareVersion(sklearnVersion, "1.5.1") > 0){
+		if(sklearnVersion != null && VersionUtil.compareVersion(sklearnVersion, "1.5.2") > 0){
 			String message = "This converter version does not know about Scikit-Learn version " + sklearnVersion + " artifacts. " +
-				"Please upgrade the converter to the latest version, or downgrade Scikit-Learn to version " + "1.5.1";
+				"Please upgrade the converter to the latest version, or downgrade Scikit-Learn to version " + "1.5.2";
 
 			throw new SkLearnException(message);
 		}
